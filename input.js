1c192d1568495b12ba970a4bdfd190e07ec2094c
NotTimIsReal <84319818+NotTimIsReal@users.noreply.github.com>
2022-03-19 18:30:33 +1100

    refactored code

--- input.js
+++ input.js
@@ -1,23 +1,23 @@
-let Inputdirection = { x: 0, y: 0 }
-let lastInputDirection = { x: 0, y: 0 }
+const Inputdirection = { x: 0, y: 0 }
+const lastInputDirection = { x: 0, y: 0 }
 
 window,addEventListener( 'keydown' , e => {
     switch (e.key) {
         case 'ArrowUp':
             if (lastInputDirection.y == 0) break
-            Inputdirection = { x: 0, y: -1 }
+            Inputdirection.y=-1
             break
          case 'ArrowDown':
             if (lastInputDirection.y == 0) break
-            Inputdirection = { x: 0, y: 1 } 
+            nputdirection.y=1
             break
          case 'ArrowLeft':
             if (lastInputDirection.y == 0) break
-            Inputdirection = { x: -1, y: 0 }
+            nputdirection.x=-1
             break
          case 'ArrowRight':
             if (lastInputDirection.y == 0) break
-            Inputdirection = { x: 1, y: 0}
+            nputdirection.y=1
             break
     }
 })
